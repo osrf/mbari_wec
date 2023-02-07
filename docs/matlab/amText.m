40b65ae3960913cd19c5c36a7d8beaa035bfb66e
Rob McEwen <rob@Blackhawk.local>
2023-02-06 16:06:45 -0800

    Updated notation.  Reduced the number of significant digits.

--- docs/matlab/amText.m
+++ docs/matlab/amText.m
@@ -7,55 +7,55 @@ if(size(MA) ~= [6 6])
 end
 
 strMat(1,1) = ...
-    "| \\(X_{\\dot{u}}\\)   | Surge Added Mass (\\(\\mu_{xx}\\))            | %12.2f  | kg        |";
+    "| \\(X_{\\dot{u}}\\)   | Surge Added Mass (\\(\\mu_{xx}\\))            | %12.f   | kg        |";
 strMat(1,2) = ...
-    "| \\(X_{\\dot{v}}\\)   | Surge-Sway Added Mass (\\(\\mu_{xy}\\))       | %12.2f  | kg        |";
+    "| \\(X_{\\dot{v}}\\)   | Surge-Sway Added Mass (\\(\\mu_{xy}\\))       | %12.f   | kg        |";
 strMat(1,3) = ...
-    "| \\(X_{\\dot{w}}\\)   | Surge-Heave Added Mass (\\(\\mu_{xz}\\))      | %12.2f  | kg        |";
+    "| \\(X_{\\dot{w}}\\)   | Surge-Heave Added Mass (\\(\\mu_{xz}\\))      | %12.f   | kg        |";
 strMat(1,4) = ...
-    "| \\(X_{\\dot{p}}\\)   | Surge-Roll Added Mass MOI (\\(\\mu_{xp}\\))   | %12.2f  | kg m      |";
+    "| \\(X_{\\dot{p}}\\)   | Surge-Roll Added Mass MOI (\\(\\mu_{xp}\\))   | %12.f   | kg m      |";
 strMat(1,5) = ...
-    "| \\(X_{\\dot{q}}\\)   | Surge-Pitch Added Mass MOI(\\(\\mu_{xq}\\))   | %12.2f  | kg m      |";
+    "| \\(X_{\\dot{q}}\\)   | Surge-Pitch Added Mass MOI (\\(\\mu_{xq}\\))  | %12.f   | kg m      |";
 strMat(1,6) = ...
-    "| \\(X_{\\dot{r}}\\)   | Surge-Yaw Added Mass MOI (\\(\\mu_{xr}\\))    | %12.2f  | kg m      |";
+    "| \\(X_{\\dot{r}}\\)   | Surge-Yaw Added Mass MOI (\\(\\mu_{xr}\\))    | %12.f   | kg m      |";
 
 strMat(2,2) = ...
-    "| \\(Y_{\\dot{v}}\\)   | Sway Added Mass (\\(\\mu_{yy}\\))             | %12.2f  | kg        |";
+    "| \\(Y_{\\dot{v}}\\)   | Sway Added Mass (\\(\\mu_{yy}\\))             | %12.f   | kg        |";
 strMat(2,3) = ...
-    "| \\(Y_{\\dot{w}}\\)   | Sway-Heave Added Mass (\\(\\mu_{yz}\\))       | %12.2f  | kg        |";
+    "| \\(Y_{\\dot{w}}\\)   | Sway-Heave Added Mass (\\(\\mu_{yz}\\))       | %12.f   | kg        |";
 strMat(2,4) = ...
-    "| \\(Y_{\\dot{p}}\\)   | Sway-Roll Added Mass MOI (\\(\\mu_{yp}\\))    | %12.2f  | kg m      |";
+    "| \\(Y_{\\dot{p}}\\)   | Sway-Roll Added Mass MOI (\\(\\mu_{yp}\\))    | %12.f   | kg m      |";
 strMat(2,5) = ...
-    "| \\(Y_{\\dot{q}}\\)   | Sway-Pitch Added Mass MOI(\\(\\mu_{yq}\\))    | %12.2f  | kg m      |";
+    "| \\(Y_{\\dot{q}}\\)   | Sway-Pitch Added Mass MOI(\\(\\mu_{yq}\\))    | %12.f   | kg m      |";
 strMat(2,6) = ...
-    "| \\(Y_{\\dot{r}}\\)   | Sway-Yaw Added Mass MOI (\\(\\mu_{yr}\\))     | %12.2f  | kg m      |";
+    "| \\(Y_{\\dot{r}}\\)   | Sway-Yaw Added Mass MOI (\\(\\mu_{yr}\\))     | %12.f   | kg m      |";
 
 
 strMat(3,3) = ...
-    "| \\(Z_{\\dot{w}}\\)   | Heave Added Mass (\\(\\mu_{yy}\\))            | %12.2f  | kg        |";
+    "| \\(Z_{\\dot{w}}\\)   | Heave Added Mass (\\(\\mu_{yy}\\))            | %12.f   | kg        |";
 strMat(3,4) = ...
-    "| \\(Z_{\\dot{p}}\\)   | Heave-Roll Added Mass MOI (\\(\\mu_{yp}\\))   | %12.2f  | kg m      |";
+    "| \\(Z_{\\dot{p}}\\)   | Heave-Roll Added Mass MOI (\\(\\mu_{yp}\\))   | %12.f   | kg m      |";
 strMat(3,5) = ...
-    "| \\(Z_{\\dot{q}}\\)   | Heave-Pitch Added Mass MOI(\\(\\mu_{yq}\\))   | %12.2f  | kg m      |";
+    "| \\(Z_{\\dot{q}}\\)   | Heave-Pitch Added Mass MOI(\\(\\mu_{yq}\\))   | %12.f   | kg m      |";
 strMat(3,6) = ...
-    "| \\(Z_{\\dot{r}}\\)   | Heave-Yaw Added Mass MOI (\\(\\mu_{yr}\\))    | %12.2f  | kg m      |";
+    "| \\(Z_{\\dot{r}}\\)   | Heave-Yaw Added Mass MOI (\\(\\mu_{yr}\\))    | %12.f   | kg m      |";
 
 
 strMat(4,4) = ...
-    "| \\(K_{\\dot{p}}\\)   | Roll Added Mass MOI (\\(\\mu_{pp}\\))         | %12.2f  | kg m\\(^2\\)|";
+    "| \\(K_{\\dot{p}}\\)   | Roll Added Mass MOI  (\\(\\mu_{pp}\\))        | %12.f   | kg m\\(^2\\)|";
 strMat(4,5) = ...
-    "| \\(K_{\\dot{q}}\\)   | Roll-Pitch Added Mass MOI(\\(\\mu_{pq}\\))    | %12.2f  | kg m\\(^2\\)|";
+    "| \\(K_{\\dot{q}}\\)   | Roll-Pitch Added Mass MOI(\\(\\mu_{pq}\\))    | %12.f   | kg m\\(^2\\)|";
 strMat(4,6) = ...
-    "| \\(K_{\\dot{r}}\\)   | Roll-Yaw Added Mass MOI (\\(\\mu_{pr}\\))     | %12.2f  | kg m\\(^2\\)|";
+    "| \\(K_{\\dot{r}}\\)   | Roll-Yaw Added Mass MOI (\\(\\mu_{pr}\\))     | %12.f   | kg m\\(^2\\)|";
 
 
 strMat(5,5) = ...
-    "| \\(M_{\\dot{q}}\\)   | Pitch Added Mass MOI(\\(\\mu_{qq}\\))         | %12.2f  | kg m\\(^2\\)|";
+    "| \\(M_{\\dot{q}}\\)   | Pitch Added Mass MOI (\\(\\mu_{qq}\\))        | %12.f   | kg m\\(^2\\)|";
 strMat(5,6) = ...
-    "| \\(M_{\\dot{r}}\\)   | Pitch-Yaw Added Mass MOI (\\(\\mu_{qr}\\))    | %12.2f  | kg m\\(^2\\)|";
+    "| \\(M_{\\dot{r}}\\)   | Pitch-Yaw Added Mass MOI (\\(\\mu_{qr}\\))    | %12.f   | kg m\\(^2\\)|";
 
 strMat(6,6) = ...
-    "| \\(N_{\\dot{r}}\\)   | Yaw Added Mass MOI (\\(\\mu_{rr}\\))          | %12.2f  | kg m\\(^2\\)|";
+    "| \\(N_{\\dot{r}}\\)   | Yaw Added Mass MOI (\\(\\mu_{rr}\\))          | %12.f   | kg m\\(^2\\)|";
 
 k=0;
 for( i=1:6 )
