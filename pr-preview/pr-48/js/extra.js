9c9f39612fdf5a86433784f9167af9b9f938ca3a
quarkytale <quarkytale@users.noreply.github.com>
2023-05-09 03:19:54 +0000

    Deploy preview for PR 48 🛫

--- pr-preview/pr-48/js/extra.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('copy', function(e){
-  var text = window.getSelection().toString().replaceAll('$ ', '');
-  e.clipboardData.setData('text/plain', text);
-  e.preventDefault();
-});
